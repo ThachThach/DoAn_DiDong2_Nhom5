c2088c44c772b2e09233bde7a555d905af21048f
hoanghuulong <hoanghuulong.krn@gamil.com>
2021-05-05 07:26:54 +0700

    adit file MainActivity

--- DoAnDiDong/app/src/main/java/com/example/doandidong/MainActivity.java
+++ DoAnDiDong/app/src/main/java/com/example/doandidong/MainActivity.java
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Adapter;
@@ -64,5 +65,11 @@ public class MainActivity extends AppCompatActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu,menu);
+        return super.onCreateOptionsMenu(menu);
     }
 }
